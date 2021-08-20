310eb1087b438e9deb440ccfb9cd191b012776cf
RyanRJyeo <ryanrj@hotmail.sg>
2021-08-20 11:51:27 +0800

    Update shorturl to use hosted link instead of localhost

--- src/modules/Url/api/urlApi.tsx
+++ src/modules/Url/api/urlApi.tsx
@@ -54,7 +54,7 @@ export const createUrl = async (url: string): Promise<UrlModel | null> => {
       const newUrlRef = urlRef.doc()
       const values = {
         originalUrl: url,
-        shortUrl: `http://localhost:3000/url?id=${newUrlRef.id}`,
+        shortUrl: `https://url-shoterner-a5494.web.app//url?id=${newUrlRef.id}`,
       }
       await newUrlRef.set(values)
       return { ...values, id: newUrlRef.id }
