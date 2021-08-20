1ca654a1cff89a86eb58e63ad6f2f96151cd0d46
RyanRJyeo <ryanrj@hotmail.sg>
2021-08-20 10:51:39 +0800

    Update Url page to use getUrl function and redirect user

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Loading.module.css'
+
+const Loading:React.FC = () => {
+    return (
+        <div className={styles.screen}>
+            <h4>Loading...</h4>
+        </div>
+    )
+}
+
+export default Loading
--- src/pages/Url/Url.tsx
+++ src/pages/Url/Url.tsx
@@ -1,7 +1,38 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import Loading from '../../components/Loading/Loading'
+import { getUrl } from '../../modules/Url/api/urlApi'
+import { UrlModel } from '../../modules/Url/model/urlModel'
 
-const Url: React.FC = () => {
-  return <div>Ello from url page</div>
+const Url: React.FC = (props) => {
+  const search = useLocation().search
+  const id = new URLSearchParams(
+    search as unknown as Record<string, string>
+  ).get('id')
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [url, setUrl] = useState<UrlModel>()
+
+  useEffect(() => {
+    const fetchData = async (id: string) => {
+      const nextUrl = await getUrl(id)
+      setUrl(nextUrl)
+      setIsLoading(false)
+    }
+
+    if (id) {
+      setIsLoading(true)
+      fetchData(id)
+    }
+  }, [])
+
+  if (isLoading) return <Loading />
+  if (url) window.location.href = url.originalUrl
+
+  return <div>No such url</div>
 }
 
 export default Url
